f2cc237071b7266a4ea00609e994eabde79b1464
Dmitry <elvs18@mail.ru>
2021-11-03 16:04:00 +0100

    Emit events

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks @delete-task="deleteTask()" :tasks="tasks" />
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
@@ -20,8 +20,16 @@ export default {
   // },
   methods: {
     deleteTask(id) {
-      console.log(task, id)
-    }
+      if (confirm('Are you sure?')){
+        console.log(this.tasks)
+        this.tasks = this.tasks.filter((task) => task.id !== id)
+        console.log(this.tasks)
+      }
+    },
+    toggleReminder(id) {
+      console.log(this.tasks)
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    },
   },
   created() {
     this.tasks = [
